4c9b1e6fcb580236e31fbbcf72c3acc431056bdc
hensetiawan <hendystyawan74@gmail.com>
2024-08-26 17:12:04 +0700

    refactor: api route

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import userRoute from "./src/routers/user.js";
-import roleRoute from "./src/routers/role.js";
-import authRoute from "./src/routers/auth.js";
-import nationalityRoute from "./src/routers/nationality.js";
-import authorRoute from "./src/routers/author.js";
-import languageRoute from "./src/routers/language.js";
-import bookRoute from "./src/routers/book.js";
+import apiRoute from "./src/routers/index.js";
 import { swaggerUi, specs } from "./swagger.js";
 import "dotenv/config";
 
@@ -17,13 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/api/v1", userRoute);
-app.use("/api/v1", roleRoute);
-app.use("/api/v1", authRoute);
-app.use("/api/v1", nationalityRoute);
-app.use("/api/v1", authorRoute);
-app.use("/api/v1", languageRoute);
-app.use("/api/v1", bookRoute);
+app.use("/", apiRoute);
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 
---
+++ src/routers/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+const apiRoute = express.Router();
+
+import userRoute from "./user.js";
+import roleRoute from "./role.js";
+import authRoute from "./auth.js";
+import nationalityRoute from "./nationality.js";
+import authorRoute from "./author.js";
+import languageRoute from "./language.js";
+import bookRoute from "./book.js";
+
+
+apiRoute.use("/api/v1", userRoute);
+apiRoute.use("/api/v1", roleRoute);
+apiRoute.use("/api/v1", authRoute);
+apiRoute.use("/api/v1", nationalityRoute);
+apiRoute.use("/api/v1", authorRoute);
+apiRoute.use("/api/v1", languageRoute);
+apiRoute.use("/api/v1", bookRoute);
+
+export default apiRoute;
